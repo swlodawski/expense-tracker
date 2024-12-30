c34d450aaa463689fcbae174ae73527fbd0d18c3
sammy wlodawski <samwlodawski@gmail.com>
2024-12-30 15:08:20 -0500

    Finished svg below header on the landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,29 @@ export default function Home() {
                     <Link href={url.app.sigin}
                     className='leading-2 mr-4 inline-flex h-[34px] items-center overflow-hidden rounded-full bg-gray-900 px-4 py-1 text-sm font-medium text-white transition hover:bg-primary/90'>
                         Sign In
-                    </Link>
+                    </Link>  
                 </div>
             </header>
+            <main>
+                <div className='absolute inset-x-0 top-[-55px] z-10 h-96 overflow-hidden text-gray-900/40 opacity-10 [mask-image:linear-gradient(to_top,transparent,white)]'>
+                <svg className='absolute inset-0 top-0 h-full w-full text-gray-900' xmlns='http://www.w3.org/2000/svg'>
+                <defs>
+                    <pattern 
+                    id='pattern'
+                    width='32'
+                    height='32'
+                    patternUnits='userSpaceOnUse'
+                    x='50%'
+                    y='100%'
+                    patternTransform='translate(0, -1'>
+                        <path d='M0 32V.5H32' fill='none' stroke='curretnColor'></path>
+                        </pattern>
+                    </defs>
+                    <rect width='100%' height='100%' fill='url(#pattern'></rect>
+                    </svg>
+                </div>
+                
+            </main>
         </div>
     
     )
