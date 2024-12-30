f92edafcf008816709026ca2e716f67b3ba808d7
sammy wlodawski <samwlodawski@gmail.com>
2024-12-30 16:31:52 -0500

    Finished that particular features highlight section.

--- app/page.tsx
+++ app/page.tsx
@@ -150,9 +150,59 @@ export default function Home() {
                                             </span>
                                             <span>Add up to 100 entries per account</span>
                                         </div>
+                                        <span className='m-1 ml-0 flex items-center text-[15px]'>
+                                                <svg 
+                                                xmlns='http://www.w3.org/2000/svg'
+                                                viewBox='0 0 24 24'
+                                                fill='currentColor'
+                                                aria-hidden='true'
+                                                className='mr-1 h-5 w-5 text-green-600'>
+
+                                            <path
+                                            fillRule='evenodd'
+                                            d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z'
+                                            clipRule='evenodd'></path>
+                                                </svg>
+                                            </span>
+                                            <span>Track subscription billing dates</span>
+                                            <span className='m-1 ml-0 flex items-center text-[15px]'>
+                                                <svg 
+                                                xmlns='http://www.w3.org/2000/svg'
+                                                viewBox='0 0 24 24'
+                                                fill='currentColor'
+                                                aria-hidden='true'
+                                                className='mr-1 h-5 w-5 text-green-600'>
+
+                                            <path
+                                            fillRule='evenodd'
+                                            d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z'
+                                            clipRule='evenodd'></path>
+                                                </svg>
+                                            </span>
+                                            <span>Choose prefereed currency display.</span>
+                                            <span className='m-1 ml-0 flex items-center text-[15px]'>
+                                                <svg 
+                                                xmlns='http://www.w3.org/2000/svg'
+                                                viewBox='0 0 24 24'
+                                                fill='currentColor'
+                                                aria-hidden='true'
+                                                className='mr-1 h-5 w-5 text-green-600'>
+
+                                            <path
+                                            fillRule='evenodd'
+                                            d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z'
+                                            clipRule='evenodd'></path>
+                                                </svg>
+                                            </span>
+                                            <span>Email support available</span>
                             </div>
+                            <a className='mt-10 block w-full rounded-md bg-white py-2 text-center text-sm font-semibold text-black hover:bg-gray-200' 
+                            href="https://app.Expense-Tracker/signup">
+                                    Start for free
+                            </a>
                         </div>
                     </div>
+
                 </div>
             </main>
         </div>
