aef05435a22c2ea805a97b6e19ac435c9594217f
sammy wlodawski <samwlodawski@gmail.com>
2024-12-30 18:25:37 -0500

    Finished landing page with links. Need to check into a loose div tag though.

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
                     x='50%'
                     y='100%'
                     patternTransform='translate(0, -1'>
-                        <path d='M0 32V.5H32' fill='none' stroke='curretnColor'></path>
+                        <path d='M0 32V.5H32' fill='none' stroke='currentColor'></path>
                         </pattern>
                     </defs>
                     <rect width='100%' height='100%' fill='url(#pattern'></rect>
@@ -210,11 +210,110 @@ export default function Home() {
                                         <span className='inline-flex text-3xl font-extrabold text-white'>$ 20</span>
                                         <span className='text-base text-gray-100'>/ year</span>
                                     </p>
+                                    <div className='mb-0 ml-1 mt-4 flex flex-col justify-center text-left text-white'>
+                                        <span className='m-1 ml-0 flex items-center  text-[15px]'>
+                                            <svg 
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            viewBox='0 0 24 24'
+                                            aria-hidden='true'
+                                            className='mr-1 h-5 w-5 text-green-600'>
+                                                <path
+                                                fillRule='evenodd'
+                                                d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z'
+                                                clipRule=''></path>
+                                            </svg>
+                                            Everything is a basic plan.
+                                        </span>
+                                        <span className='m-1 ml-0 flex items-center  text-[15px]'>
+                                            <svg 
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            viewBox='0 0 24 24'
+                                            aria-hidden='true'
+                                            className='mr-1 h-5 w-5 text-green-600'>
+                                                <path
+                                                fillRule='evenodd'
+                                                d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z'
+                                                clipRule=''></path>
+                                            </svg>
+                                            <span>Add up to {premiumPlan.limit} entries per account</span>
+                                        </span>
+                                        <span className='m-1 ml-0 flex items-center  text-[15px]'>
+                                            <svg 
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            viewBox='0 0 24 24'
+                                            aria-hidden='true'
+                                            className='mr-1 h-5 w-5 text-green-600'>
+                                                <path
+                                                fillRule='evenodd'
+                                                d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z'
+                                                clipRule=''></path>
+                                            </svg>
+                                            <span>Advanced trend visualization.</span>
+                                        </span>
+                                        <span className='m-1 ml-0 flex items-center  text-[15px]'>
+                                            <svg 
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            viewBox='0 0 24 24'
+                                            aria-hidden='true'
+                                            className='mr-1 h-5 w-5 text-green-600'>
+                                                <path
+                                                fillRule='evenodd'
+                                                d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z'
+                                                clipRule=''></path>
+                                            </svg>
+                                            <span>Export data as CSV</span>
+                                        </span>
+                                        <span className='m-1 ml-0 flex items-center  text-[15px]'>
+                                            <svg 
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            viewBox='0 0 24 24'
+                                            aria-hidden='true'
+                                            className='mr-1 h-5 w-5 text-green-600'>
+                                                <path
+                                                fillRule='evenodd'
+                                                d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z'
+                                                clipRule=''></path>
+                                            </svg>
+                                            <span>Priority support with quick reply</span>
+                                        </span>
+                                    </div>
+                                    <a  
+                                    className=''
+                                    href="https://app.Expense-Tracker/signup">
+                                        Get Started
+                                    </a>
                                 </div>
                             </div>
                 </div>
+                <div className=''>
+                    <h2 className=''>
+                        Proudly {' '}
+                        <span className=''>
+                            Open Sourced
+                        </span>
+                    </h2>
+                    <p className=''>
+                        Source Code is on gitbuh
+                    </p>
+                    <Link
+                    href={url.github}
+                    className='mt-6 inline-flex h-[36px] items-center justify-center  rounded-full bg-white/0 px-4 py-2.5 text-sm font-medium text-slate-900 ring-1 ring-slate-900/10 hover:bg-gray-100 hover:shadow'>
+                        <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='16'
+                        height='16'
+                        className='mr-2'
+                        fill='currentColor'
+                        viewBox='0 0 24 24'>
+                            <path 
+                            d='M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z'></path>
+                        </svg>
+                        Star on github
+                    </Link>
+                </div>
             </main>
+            <Footer />
         </div>
     
-    )
+    );
 }
